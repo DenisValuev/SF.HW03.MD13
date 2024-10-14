8fbe79a5cf542ee451adfe3cee096bbbccc484aa
DenisValuev <valuev.denis91@mail.ru>
2024-10-14 15:53:20 +0300

    Добавьте файлы проекта.

---
+++ SF.HW03.MD13.Task1.TestLinkedList/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace SF.HW03.MD13.Task1.TestLinkedList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ SF.HW03.MD13.Task1.TestList/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace SF.HW03.MD13.Task1.TestList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
