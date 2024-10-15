5666044b22927adc60ed23a6e395e2317069cc58
DenisValuev <valuev.denis91@mail.ru>
2024-10-15 16:33:38 +0300

    SF.HW03.MD13-master: Сравнение производительности операций вставки в List<> и LinkedList<>

--- SF.HW03.MD13.Task1.TestLinkedList/Program.cs
+++ SF.HW03.MD13.Task1.TestLinkedList/Program.cs
@@ -1,10 +1,51 @@
-﻿namespace SF.HW03.MD13.Task1.TestLinkedList
+﻿using System.Diagnostics;
+
+namespace SF.HW03.MD13.Task1.TestLinkedList
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Читаем весь файл в строку
+            string text = File.ReadAllText("C:\\Users\\dvalu\\OneDrive\\Документы\\SF\\SF.HW03.MD13\\Text.txt");
+
+            //Сохраним символы разделители в массив
+            char[] delimiters = new char[] { ' ', '\r', '\n' };
+
+            //Разюиваем строку текса, изпользуя символы-разделители
+            var words = text.Split(delimiters, StringSplitOptions.RemoveEmptyEntries);
+
+            #region New linkedList
+            //Запускаем новый таймер
+            var stopWatch = Stopwatch.StartNew();
+
+            //Создаем  список с типом данный string и добавляем в список массив words
+            var textTestLinkedList = new LinkedList<string>(words);
+
+            //Вывод на экран время выполнения операции в миллисекундак
+            Console.WriteLine("Время выполнения операции: " + stopWatch.Elapsed.TotalMilliseconds);
+            #endregion
+
+            #region AddAfterFirst
+            stopWatch = Stopwatch.StartNew();
+
+            textTestLinkedList.AddAfter(textTestLinkedList.First, "TEST");
+
+            Console.WriteLine("Время выполнения вставки после первого элемента: " + stopWatch.Elapsed.TotalMilliseconds);
+            #endregion
+
+            #region AddAfterLast
+            stopWatch = Stopwatch.StartNew();
+
+            textTestLinkedList.AddAfter(textTestLinkedList.Last, "TEST");
+
+            Console.WriteLine("Время выполнения вставки после последнего элемента: " + stopWatch.Elapsed.TotalMilliseconds);
+            #endregion
+            //Вычисляем размер коллекции
+            int countTestLinkedList = textTestLinkedList.Count;
+            Console.WriteLine($"Размер двухсвязного списка: {countTestLinkedList}");
+
+            Console.ReadKey();
         }
     }
 }
--- SF.HW03.MD13.Task1.TestList/Program.cs
+++ SF.HW03.MD13.Task1.TestList/Program.cs
@@ -1,10 +1,63 @@
-﻿namespace SF.HW03.MD13.Task1.TestList
+﻿using System.Diagnostics;
+
+namespace SF.HW03.MD13.Task1.TestList
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            //Читаем весь файл в строку
+            string text = File.ReadAllText("C:\\Users\\dvalu\\OneDrive\\Документы\\SF\\SF.HW03.MD13\\Text.txt");
 
+            //Сохраним символы разделители в массив
+            char[] delimiters = new char[] { ' ', '\r', '\n' };
+
+            //Разюиваем строку текса, изпользуя символы-разделители
+            var words = text.Split(delimiters, StringSplitOptions.RemoveEmptyEntries);
+           
+            //Создаем пустой список с типом данный string
+            var textTestList = new List<string>();
+
+            #region AddRange list
+            //Запускаем новый таймер
+            var stopWatch = Stopwatch.StartNew();
+
+            //Добавляем в список массив words
+            textTestList.AddRange(words);
+
+            //Вывод на экран время выполнения операции в миллисекундак
+            Console.WriteLine("Время заполнения списка: " + stopWatch.Elapsed.TotalMilliseconds);
+            #endregion
+
+            #region Insert in the middle
+            //Вычисляем размер коллекции
+            int countTestList = textTestList.Count;
+            Console.WriteLine($"Размер двухсвязного списка: {countTestList}");
+
+            int halfCount = countTestList / 2;
+
+            //Запускаем новый таймер
+            Stopwatch.StartNew();
+
+            //Добавляем в список массив words
+            textTestList.Insert(halfCount, "TEST");
+
+            //Вывод на экран время выполнения операции в миллисекундак
+            Console.WriteLine("Время выполнения вставки в середину коллекции: " + stopWatch.Elapsed.TotalMilliseconds);
+            #endregion
+
+            #region Insert at the end list
+            Stopwatch.StartNew();
+
+            //Добавляем в список массив words
+            textTestList.Add("TEST");
+
+            //Вывод на экран время выполнения операции в миллисекундак
+            Console.WriteLine("Время добавления в конец коллекции: " + stopWatch.Elapsed.TotalMilliseconds);
+            #endregion
+
+
+            Console.ReadKey();
         }
     }
 }
